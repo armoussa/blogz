aa8af7d965846d6f858ff56652a67519f5cbb2dd
Aida <aida.moussalli@gmail.com>
2018-04-26 13:46:12 -0500

    I think im finished woah

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:launchcode@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-
+app.secret_key ="allthesecrets"
 
 class Blog(db.Model):
 
@@ -27,50 +27,65 @@ class User(db.Model):
     password = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='owner')
 
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    users = User.query.all()
+    
+    return render_template('index.html',users=users)
 
-  return redirect("/blog")
+    
+
 
 @app.route('/blog',methods=['POST','GET']) 
 def blog():
     blog_id = request.args.get('id')
+    user_id= request.args.get(('userid'))
     posts = Blog.query.all()
 
     if blog_id:
         post = Blog.query.filter_by(id=blog_id).first()
-        return render_template("solopost.html",title=post.title, body=post.body)
-
+        return render_template("solopost.html",title=post.title, body=post.body, user=post.owner.username, user_id=post.owner_id)
+    if user_id:
+        entries = Blog.query.filter_by(owner_id=user_id).all()
+        return render_template('user.html',entries=entries)
 
     return render_template('blog.html',posts=posts)
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    user_error = ""
+    pass_error = ""
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
 
-        user_error = ""
-        pass_error = ""
+        
 
         if user and user.password == password:
             session['username'] = username
-            flash("Logged in")
             return redirect('/newpost')
         if not user:
             user_error = "Username does not exist."
-            return(render_template('login.html',user_error=user_error)
-
-            #flash('User password incorrect, or user does not exist', 'error')
+            return render_template('login.html',user_error=user_error)
         else:
-            pass_error = "Your username or password was incorrect."
-            return(render_template('login.html',pass_error=pass_error)
-            # username or password is incorrect    
+            pass_error= "Your username or password was incorrect."
+            return render_template('login.html',pass_error=pass_error) 
 
     return render_template('login.html')
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login','blog','index','signup']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
@@ -150,6 +165,11 @@ def newpost():
                 body_error = body_error
             )
     return render_template("newpost.html")
+
+@app.route('/blog')
+def logout():
+    del session['username']
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
